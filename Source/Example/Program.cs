34ae104387ad63cfc1713922a19d702c1c0e3d76
Daniël <daniel@dutchgrit.nl>
2024-02-05 15:48:10 +0100

    feat: basic implementation

---
+++ Source/Example/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var sizes = Console.ReadLine().Split(" ");
+var height = Int32.Parse(sizes[0]);
+var width = Int32.Parse(sizes[1]);
+
+var arr = new List<int[]>();
+
+for (int i = 0; i < height; i++)
+{
+   var data = Console.ReadLine()!.Split(" ").Select(c=> Int32.Parse(c)).Select(c => c != 0 ? 0 : -1).ToArray();
+   arr.Add(data);
+}
+
+var rnd = new Random();
+while(true)
+{
+
+    Thread.Sleep(10);
+    var roll = Console.ReadLine();
+    if (roll == null) continue;
+    
+    var nums = roll.Split(" ");
+    var first = Int32.Parse(nums[1]);
+    var second = Int32.Parse(nums[2]);
+
+    var x = rnd.Next(0, width / 2);
+    var y = rnd.Next(0, height);
+    while (arr[y][x] != 0 || arr[y][x] == -1)
+    {
+        x = rnd.Next(0, width / 2);
+        y = rnd.Next(0, height);
+    }
+    
+    arr[y][x] = first;
+    Console.WriteLine($"p {first} {x},{y}");
+}
---
+++ Source/Tester/Program.cs
@@ -0,0 +1,113 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[][] grid = [
+    [0, 0, 1, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [2, 1, 1, 1, 1, 1, 1, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 1, 1, 1, 1, 0, 0],
+];
+
+
+// im sure theres a simpler way
+int[][] state = grid.Select((r) => r.Select(f => 0).ToArray()).ToArray();
+int games = grid.Select(f => f.Count(c => c != 0)).Sum() / 2;
+
+Console.WriteLine($"Playing {games} Games");
+
+Console.WriteLine(Environment.CurrentDirectory);
+
+var currentDice = (2, 4);
+
+Random rnd = new Random();
+
+System.Diagnostics.Process process = new System.Diagnostics.Process();
+System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+startInfo.WorkingDirectory = Environment.CurrentDirectory.Replace("\\Tester\\bin\\Debug\\net8.0", "");
+startInfo.FileName = "cmd.exe";
+startInfo.Arguments = "/C dotnet run --project Example";
+process.StartInfo = startInfo;
+process.StartInfo.UseShellExecute = false;
+process.StartInfo.RedirectStandardOutput = true;
+process.StartInfo.RedirectStandardInput = true;
+process.OutputDataReceived += (sender, args) =>
+{
+    Console.WriteLine(args.Data);
+    var data = args.Data;
+
+    if (!data.StartsWith("p"))
+    {
+        Console.WriteLine($"Malformed data received {data}");
+        //return early
+        return;
+    }
+
+
+    var items = data.Split(" ");
+    var choice = Int32.Parse(items[1]);
+    var location = items[2].Split(",").Select(c => Int32.Parse(c)).ToArray();
+
+    if (choice != currentDice.Item1 && choice != currentDice.Item2)
+    {
+        Console.WriteLine($"{choice} was not rolled please try again!");
+        return;
+    }
+
+    games -= 1;
+
+
+    state[location[1]][location[0]] = choice;
+    state[location[1]][grid[0].Length - location[0] - 1] = choice == currentDice.Item1 ? currentDice.Item2 : currentDice.Item1;
+
+    var done = !state.Select(f => f.FirstOrDefault(f => f == 0, -1)).Contains(0);
+    
+    if (games == 0)
+    {
+        Console.WriteLine("Done!!!");
+        foreach (var row in state)
+        {
+            Console.WriteLine(String.Join(" ", row));
+        }
+        // calculate points
+        
+
+
+        //DIE!
+        process.Kill();
+        Environment.Exit(1);
+
+    }
+
+    foreach (var row in state)
+    {
+        Console.WriteLine(String.Join(" ", row));
+    }
+
+    currentDice = (
+        rnd.Next(1, 6),
+        rnd.Next(1, 6)
+    );
+
+    process.StandardInput.WriteLine($"d {currentDice.Item1} {currentDice.Item2}");
+};
+process.Start();
+process.BeginOutputReadLine();
+
+process.StandardInput.WriteLine($"{grid.Length} {grid[0].Length}");
+foreach (var row in grid)
+{
+    process.StandardInput.WriteLine(String.Join(" ", row));
+}
+
+process.StandardInput.WriteLine($"d {currentDice.Item1} {currentDice.Item2}");
+
+
+// This places 2 on 2,2 and also makes it so 4 is placed on 7,2
+// input message example p 2 2,2
+
+await process.WaitForExitAsync();
+
